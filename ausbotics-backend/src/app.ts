51ee36c935fbe2abcbcb1b10874e2b66e02e2d21
Ashish mehta <ashishmehtawork108@gmail.com>
2025-09-16 20:09:23 +0530

    added morgan for apis consoles

--- ausbotics-backend/src/app.ts
+++ ausbotics-backend/src/app.ts
@@ -4,7 +4,6 @@ import authRoutes from "./routes/auth.routes";
 import usersRoutes from "./routes/users.routes";
 import workflowsRoutes from "./routes/workflows.routes";
 import resultsRoutes from "./routes/results.routes";
-import dummyRouter from "./routes/dummy.routes";
 import { errorHandler } from "./middlewares/error.middleware";
 import morgan from "morgan";
 import appointmentRouter from "./routes/appointments.routes";
@@ -27,7 +26,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/workflows", workflowsRoutes);
 app.use("/api/results", resultsRoutes);
-app.use("/api/dummy", dummyRouter);
 app.use("/api/appointments", appointmentRouter);
 app.use("/api/health", (req, res) => {
   res.status(200).json({ message: "ok" });
